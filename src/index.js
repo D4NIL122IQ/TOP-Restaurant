d3becfee58a8976d7ca10f99cbd1c1715327a48a
Danil gdj <danilguidjou7@gmail.com>
2024-06-20 15:58:25 +0100

    js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+import {creatFormContact} from './contact'
+
 const navigationButtons = document.querySelectorAll("button")
 const container = document.querySelector("#container")
 
 
 navigationButtons.forEach((btn)=>{
     btn.addEventListener("click" , ()=>{
-        container.innerHTML =""
-        let div = document.createElement("div")
-        div.innerHTML = `<h1>${btn.className}</h1>`
-        container.appendChild(div)
+        container.innerHTML = ""
+        
+        if (btn.className == "Contact"){
+            
+            creatFormContact(container)
+        }else{
+            let div = document.createElement("div")
+            div.innerHTML = `<h1>${btn.className}</h1>`
+            container.appendChild(div)
+        }
+       
+        
+        
     })
 })
