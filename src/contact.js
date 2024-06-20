a307321ca30e4985537e97a10c18e5c9e5c17dde
Danil gdj <danilguidjou7@gmail.com>
2024-06-20 15:58:20 +0100

    creation d'un formulaire pour la section contact

---
+++ src/contact.js
@@ -0,0 +1,24 @@
+import './styleForm.css'
+function creatFormContact(container){
+    let form = document.createElement("form")
+    let fullName = document.createElement('input')
+    fullName.type = 'text'
+    fullName.name ='fullName'
+    fullName.id ='fullName'
+    fullName.placeholder = 'Full Name'
+
+    let email = document.createElement("input")
+    email.type = 'email'
+    email.id ='email'
+    email.placeholder = 'exemple@mail.com'
+
+    let sub = document.createElement('input')
+    sub.type = 'submit'
+    sub.value = 'Commander'
+    form.appendChild(fullName)
+    form.appendChild(email)
+    form.appendChild(sub)
+    container.appendChild(form)
+}
+
+export{creatFormContact}
