7a2b43a3d54e6b063c3d014ce6192fe09dd57d8a
Danil gdj <danilguidjou7@gmail.com>
2024-06-20 03:05:38 +0100

    js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-document.querySelector('h1').innerHTML += "je suis le jss"
-console.log("hello lemonde")
+const navigationButtons = document.querySelectorAll("button")
+const container = document.querySelector("#container")
+
+
+navigationButtons.forEach((btn)=>{
+    btn.addEventListener("click" , ()=>{
+        container.innerHTML =""
+        let div = document.createElement("div")
+        div.innerHTML = `<h1>${btn.className}</h1>`
+        container.appendChild(div)
+    })
+})
