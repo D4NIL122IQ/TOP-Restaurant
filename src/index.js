160f7f83420cba23784230353cfec2623f5091d6
Danil gdj <danilguidjou7@gmail.com>
2024-06-21 03:51:03 +0100

    js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {creatFormContact} from './js/contact.js'
 import {creatHomepage} from './js/home.js'
+import { creatAbout } from './js/about.js'
 import './css/general.css'
 import shLogo from './image/logo.png'
 
@@ -20,20 +21,19 @@ logo.height = 50
 document.querySelector(".logo").appendChild(logo)
 
 creatHomepage(container)
+
 navigationButtons.forEach((btn)=>{
     btn.addEventListener("click" , ()=>{
         container.innerHTML = ""
         
-        if (btn.className == "Contact"){
+        if (btn.className == "Home"){
             
+            creatHomepage(container)
+        }else if (btn.className == "Contact"){
             creatFormContact(container)
-        }else{
-            let div = document.createElement("div")
-            div.innerHTML = `<h1>${btn.className}</h1>`
-            container.appendChild(div)
+        }else if (btn.className == "About"){
+            creatAbout(container)
         }
        
-        
-        
     })
 })
---
+++ src/js/about.js
@@ -0,0 +1,41 @@
+import '../css/styleAbout.css'
+
+function creatAbout(div0) {
+
+    let div = document.createElement('div')
+    div.classList.add('us')
+
+    let h1 = document.createElement('h1')
+    h1.textContent = "C'est qui DAWASUSHI"
+    let p0 = document.createElement('p')
+    p0.textContent = `Dawasushi incarne l'essence de la gastronomie japonaise authentique. 
+                Niché au cœur de la ville, notre restaurant est un sanctuaire où l'art du sushi et les saveurs japonaises
+                se rencontrent avec passion et précision. Chaque plat est une ode à la tradition, préparé par nos chefs talentueux
+                qui maîtrisent les techniques séculaires tout en explorant de nouvelles saveurs modernes. `
+    let p1 = document.createElement('p')
+    p1.textContent = `Nous nous engageons fermement envers la qualité, 
+                    en sélectionnant soigneusement les meilleurs ingrédients. Chaque poisson frais,
+                    chaque légume croquant est choisi avec attention pour garantir une expérience gustative inoubliable à chaque visite.`
+    let p2 =document.createElement('p')
+    p2.textContent =`Bienvenue chez Dawasushi, où chaque repas devient une célébration de la richesse culinaire du Japon.
+                    Venez savourer une expérience authentique dans une ambiance accueillante où chaque détail est pensé pour 
+                    vous offrir une immersion complète dans la cuisine japonaise.`
+    let p3 =document.createElement('p')
+    p0.textContent =`Ne tardez plus et reservez une place au sein de notre reataurant a l'adresse suivente`
+    let h2 = document.createElement('h2')
+    h2.textContent = `Rue des freres tifaoui, Dawadji, Bejaia, 06000 `
+    let h4 = document.createElement('h4')
+    h4.textContent = `DAWASUSHI = dawadji sushi`
+
+    div.appendChild(h1)
+    div.appendChild(p0)
+    div.appendChild(p1)
+    div.appendChild(p2)
+    div.appendChild(p3)
+    div.appendChild(h2)
+    div.appendChild(h4)
+
+    div0.appendChild(div)
+}
+
+export {creatAbout}
